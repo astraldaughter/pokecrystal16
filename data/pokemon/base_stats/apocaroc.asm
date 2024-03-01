e3c3e716194b0604524e138d80320fd283f1e682
astraldaughter <108581043+astraldaughter@users.noreply.github.com>
2024-03-01 23:36:22 +0000

    forgot oneeeee
    forgot to put apocaroc's base stats in the last commit xdddd

---
+++ data/pokemon/base_stats/apocaroc.asm
@@ -0,0 +1,21 @@
+	db 0 ; species ID placeholder
+
+	db  60,  72,  120,  48,  72,  108
+	;   hp  atk  def  spd  sat  sdf
+
+	db GROUND, PSYCHIC ; type
+	db 90 ; catch rate
+	db 168 ; base exp
+	db NO_ITEM, NO_ITEM ; items
+	db GENDER_UNKNOWN ; gender ratio
+	db 100 ; unknown 1
+	db 20 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/apocaroc/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_MINERAL, EGG_MINERAL ; egg groups
+
+	; tm/hm learnset
+	tmhm
+	; end
